3c3ff02f30f8aadb5e2328ec91d38f9377ed99a2
hale <wuhao0015@qq.com>
2018-12-05 10:48:04 +0800

    ver 2

--- matplot/final_3_添加注释.py
+++ matplot/final_3_添加注释.py
@@ -1,20 +1,15 @@
 import matplotlib.pylab as plt
 import final_0_data
 
-fig = plt.figure(figsize=(10, 5), dpi=80)
-#figsize为英寸
-#dpi为分辨率
+fig = plt.figure()
 
-ax = fig.add_subplot(211) #两排，每排一张图，第一张图
+ax = fig.add_subplot(111)
 
 import matplotlib.dates as mdate
-ax.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d %H:%M:%S'))
-#ax.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M'))
-
+ax.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M'))
 
 import matplotlib.ticker as mtick
-ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f%%'))
-#ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%d%%')) #最前和最后的%为固定格式
+ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%d%%'))
 
 X = []
 Y = []
@@ -24,6 +19,14 @@ for (time, cpu) in final_0_data.R1_CPU_TIME:
     Y.append(cpu)
 
 fig.autofmt_xdate() #自动调整X轴格式
+
+#########################注释1#########################
+#plt.rcParams['font.sans-serif'] = ['SimHei']
+#plt.title('路由器CPU使用率')
+#plt.xlabel('采集时间')
+#plt.ylabel('CPU使用率')
+#########################注释1#########################
+
 
 plt.plot(X, Y) #制图
 plt.show() #显示图
