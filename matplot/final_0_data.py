d4845474dceb4feb27d0590e3fca6ba074099f62
hale <wuhao0015@qq.com>
2018-12-05 09:50:11 +0800

    ver 2

--- matplot/final_0_data.py
+++ matplot/final_0_data.py
@@ -1,4 +1,22 @@
 import random
 import datetime
 
-R1
+R1_CPU_TIME = []
+
+now = datetime.datetime.now()
+
+for i in range(-12, 13):
+    time = (datetime.datetime.now() + datetime.timedelta(hours=i))
+    R1_CPU_TIME.append((time, random.randint(1, 100)))
+
+print(R1_CPU_TIME)
+
+R2_CPU_TIME = []
+
+now = datetime.datetime.now()
+
+for i in range(-12, 13):
+    time = (datetime.datetime.now() + datetime.timedelta(hours=i))
+    R2_CPU_TIME.append((time, random.randint(0, 100)))
+
+print(R2_CPU_TIME)
---
+++ matplot/final_2_时间为X百分比为Y.py
@@ -0,0 +1,27 @@
+import matplotlib.pylab as plt
+import final_0_data
+
+fig = plt.figure(figsize=(10, 5), dpi=80)
+#figsize为英寸
+#dpi为分辨率
+
+ax = fig.add_subplot(211) #两排，每排一张图，第一张图
+
+import matplotlib.dates as mdate
+ax.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d %H:%M:%S'))
+#ax.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M'))
+
+import matplotlib.ticker as mtick
+ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f%%'))
+#ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%d%%')) #最前和最后的%为固定格式
+
+X = []
+Y = []
+
+for (time, cpu) in final_0_data.R1_CPU_TIME:
+    X.append(time)
+    Y.append(cpu)
+
+fig.autofmt_xdate() #自动调整X轴格式
+plt.plot(X, Y)
+plt.show()
